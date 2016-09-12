49e57a529dd1b272c9ecde7ab8061caaf2694765
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-09-12 17:47:50 -0600

    Added User model
    -migrations set for user model and test

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'shoulda-matchers'
 
+
 end
 
 group :development do
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+
+RSpec.describe User, type: :model do
+  it "can be created with proper params" do
+    user = User.new(username: "user", password: "password")
+
+    expect(user).to eq(user)
+  end
+
+  context "validations" do
+    it { is_expected.to validate_presence_of(:username) }
+    it { is_expected.to validate_uniqueness_of(:username) }
+    it { is_expected.to validate_presence_of(:password) }
+  end
+end
