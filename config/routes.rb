af6bcbf779e4c384a1488cce74e789485e72973c
Daniel Broadbent <dbroadbent85@gmail.com>
2016-09-13 14:56:31 -0600

    Adds basic cart functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   resources :categories, only: [:show]
 
+  resources :carts, only: [:create, :index]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
