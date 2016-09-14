ecbc6903deef8c428039027ccf30f91590488c9d
Ryan Workman <rdavid1099@gmail.com>
2016-09-14 13:15:23 -0600

    Add user login and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,6 @@ Rails.application.routes.draw do
   post '/cart', to: 'cart#create'
   delete '/cart', to: 'cart#destroy'
   resources :carts, only: [:create, :index]
-<<<<<<< HEAD
-
-=======
->>>>>>> d9cce23341143961a21a4b8823e67e2200282568
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
