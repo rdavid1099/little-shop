753ae05b74785a65d87a3e97abec139415201d70
Ryan Workman <rdavid1099@gmail.com>
2016-09-14 16:38:18 -0600

    Add functionality forcing users to log in in order to check out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to dashboard_path
+      @cart.empty? ? redirect_to(dashboard_path) : redirect_to(cart_path)
     else
       flash.now[:error] = @user.errors.full_messages.join(', ')
       render :new
---
+++ spec/features/guest_user_adds_items_to_cart_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "guest user adds items to cart" do
+  scenario "guest user adds items to cart and logins in to checkout" do
+    make_trip('Bermuda')
+    make_trip('Basement')
+    trip1 = Trip.first
+    trip2 = Trip.last
+    visit trip_path(trip1)
+    click_on 'Add to Cart'
+    visit trip_path(trip2)
+    click_on 'Add to Cart'
+    click_on 'Login or Create Account to Checkout'
+    click_on 'Create Account'
+    fill_in 'user_name', with: 'Test Tester'
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_username', with: 'test'
+    fill_in 'user_password', with: 'password'
+    click_on 'Create Account'
+
+    expect(page).to have_content('1 - Bermuda - $199.99')
+    expect(page).to have_content('1 - Basement - $199.99')
+    expect(page).to have_content('Logged in as test')
+    expect(page).to have_content('Checkout')
+  end
+end
