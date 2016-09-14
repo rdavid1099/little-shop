e5b3b65b6a63e1d7cf3e4dd4f939db5c3813b1c3
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-09-14 15:12:54 -0600

    Fixed double quotes update from hound

--- spec/features/visitor_can_change_cart_quantity_spec.rb
+++ spec/features/visitor_can_change_cart_quantity_spec.rb
@@ -8,9 +8,9 @@ RSpec.feature 'Visitor who has items in cart' do
 
     expect(page).to have_content('1 - Bermuda - $199.99')
 
-    click_on "Edit Quantity"
-    fill_in "Quantity", with: "2"
-    click_on "Update Quantity"
+    click_on 'Edit Quantity'
+    fill_in 'Quantity', with: '2'
+    click_on 'Update Quantity'
 
     expect(page).to have_content('2 - Bermuda - $399.98')
     expect(page).to have_content('Total: $399.98')
@@ -23,16 +23,16 @@ RSpec.feature 'Visitor who has items in cart' do
 
     visit '/cart'
 
-    click_on "Edit Quantity"
-    fill_in "Quantity", with: "2"
-    click_on "Update Quantity"
+    click_on 'Edit Quantity'
+    fill_in 'Quantity', with: '2'
+    click_on 'Update Quantity'
 
     expect(page).to have_content('2 - Bermuda - $399.98')
     expect(page).to have_content('Total: $399.98')
 
-    click_on "Edit Quantity"
-    fill_in "Quantity", with: "1"
-    click_on "Update Quantity"
+    click_on 'Edit Quantity'
+    fill_in 'Quantity', with: '1'
+    click_on 'Update Quantity'
 
     expect(page).to have_content('1 - Bermuda - $199.99')
     expect(page).to have_content('Total: $199.99')
