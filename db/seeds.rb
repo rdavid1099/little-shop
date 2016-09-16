73d584a6033ab2507280b7082a5824e6580f495c
Ryan Workman <rdavid1099@gmail.com>
2016-09-16 17:50:51 -0600

    Added admin to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,5 @@
 Trip.create(title: "Kenny's Parents' Basement", description: "Enjoy a luxurious weekend at Kenny's Parents' basement.", price: '199.99', image: 'http://1.bp.blogspot.com/-RDs5O3YHALM/UHx7YK5To9I/AAAAAAAAAQE/8fZO1Gthoww/s1600/Basement.jpg')
 Trip.create(title: 'Tea Ski', description: "Sit in the lap of luxury.  Enjoy a relaxing weekend water skiing in a freshly steeped pond.", price: '599.99', image: 'http://www.showskimag.com/images/gallery/shoeski_fail.jpg')
 Trip.create(title: 'Treehouse Getaway', description: "Who said treehouses are only for kids.  You and your significant other can relive the days of old climbing up and down this luxurious bungalow.", price: '299.99', image: 'https://johnfudrow.files.wordpress.com/2008/08/treehouse.jpg')
+
+User.create(name: 'Admin Bob', username: 'admin', password: '@dminPa$$word', email: 'admin@email.com', role: 1)
