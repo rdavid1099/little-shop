941d7e1684c8007df5a3788307240e76f6a62510
Daniel Broadbent <dbroadbent85@gmail.com>
2016-09-17 11:58:10 -0600

    Adds feature admin cannot modify other user accounts

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,6 +2,6 @@ class BaseController < ApplicationController
   before_action :validate_user
 
   def validate_user
-    render file: 'public/404' unless current_user
+    render file: 'public/404' if current_user.nil? || current_user.admin?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit, :update]
 
   resources :trips, only: [:index, :show]
 
