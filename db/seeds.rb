9650e89d21ce4a273afd18b641eaf9987c109db8
Ryan Workman <rdavid1099@gmail.com>
2016-09-19 16:58:17 -0600

    Refactor Views (#63)
    * added seed data
    
    * Fix git issue involving Dan's computer
    
    * Fix formatting issue with items index
    
    * added cart to nav bar
    
    * added cart to nav bar (#53)
    
    * bootstrapping and seeding
    
    * new bootstrap template for trips index and categories (#61)
    
    * Changed to active record methods to find top sellers (#62)
    
    -Used join table on trips to get order trips
    
    * Fix hound issues
    
    * Fix hound issues again

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 trips = Trip.create([
-  {title: "Kenny's Parents' Basement", description: "Enjoy a luxurious weekend at Kenny's Parents' basement.", price: '199.99', image: 'http://1.bp.blogspot.com/-RDs5O3YHALM/UHx7YK5To9I/AAAAAAAAAQE/8fZO1Gthoww/s1600/Basement.jpg'},
-  {title: 'Tea Ski', description: "Sit in the lap of luxury.  Enjoy a relaxing weekend water skiing in a freshly steeped pond.", price: '599.99', image: 'http://www.showskimag.com/images/gallery/shoeski_fail.jpg'},
-  {title: 'Treehouse Getaway', description: "Who said treehouses are only for kids.  You and your significant other can relive the days of old climbing up and down this luxurious bungalow.", price: '299.99', image: 'https://johnfudrow.files.wordpress.com/2008/08/treehouse.jpg'},
-  {title: 'Spooky Forest', description: "It doesn't have to be Halloween to have a spooky getaway.  Be one with the wilderness at this creepy destination, if you dare.", price: '666.00', image: 'https://upload.wikimedia.org/wikipedia/commons/2/27/Sunny_Lenga_Forest_%283321050068%29.jpg'},
-  {title: 'Slippery Slope', description: "If you take this vacation, you'll buy everything else we have to offer.  See why everyone says there's no coming back from this luxurious destination.", price: '111.11', image: 'https://yandoo.files.wordpress.com/2014/02/slippery-slope.jpg'},
-  {title: 'New Snork City', description: "Come along with Snorks.  Swim along with Snorks.  So much to see waiting for you and me.  Have some fun with the Snorks", price: '800.88', image: 'http://vignette1.wikia.nocookie.net/snorks/images/3/33/Snorks_119_1.JPG/revision/latest/scale-to-width-down/1000?cb=20110812220553'},
-  {title: 'Island Adventure Park', description: "Be one of the first to see this state of the art adventure park.  What could possibli go wrong?", price: '1999.95', image: 'http://vignette2.wikia.nocookie.net/jurassicpark/images/f/fe/Gate_Entrance_(2).JPG/revision/latest?cb=20121109061245'},
-  {title: 'Space Travel Experience', description: "Join legendary captain Mark Watney on this once in a lifetime opportunity to be one of the first civilians to colonize Mars.", price: '99999.95', image: 'https://lovelace-media.imgix.net/uploads/36/85de1950-48ee-0133-9dc2-0af7184f89fb.jpg'},
-  {title: 'Hotel Winter Housekeeper', description: "FREE! Come stay the winter at the beautiful Stanley hotel in room 237. Just keep the place in one piece and make sure you play; don't do all work.", price: '0', image: 'http://www.themarysue.com/wp-content/uploads/2014/07/OverlookHotelShining.png'},
+  {title: "Kenny's Parents' Basement", description: "Enjoy a luxurious weekend at Kenny's Parents' basement.", price: '199.99', image: '/assets/Basement.jpg'},
+  {title: 'Tea Ski', description: "Sit in the lap of luxury.  Enjoy a relaxing weekend water skiing in a freshly steeped pond.", price: '599.99', image: '/assets/shoeski_fail.jpg'},
+  {title: 'Treehouse Getaway', description: "Who said treehouses are only for kids.  You and your significant other can relive the days of old climbing up and down this luxurious bungalow.", price: '299.99', image: '/assets/treehouse.jpg'},
+  {title: 'Spooky Forest', description: "It doesn't have to be Halloween to have a spooky getaway.  Be one with the wilderness at this creepy destination, if you dare.", price: '666.00', image: '/assets/Sunny_Lenga_Forest_%283321050068%29.jpg'},
+  {title: 'Slippery Slope', description: "If you take this vacation, you'll buy everything else we have to offer.  See why everyone says there's no coming back from this luxurious destination.", price: '111.11', image: '/assets/slippery-slope.jpg'},
+  {title: 'New Snork City', description: "Come along with Snorks.  Swim along with Snorks.  So much to see waiting for you and me.  Have some fun with the Snorks", price: '800.88', image: '/assets/Snorks_119_1.jpg'},
+  {title: 'Island Adventure Park', description: "Be one of the first to see this state of the art adventure park.  What could possibli go wrong?", price: '1999.95', image: '/assets/jurassic.jpg'},
+  {title: 'Space Travel Experience', description: "Join legendary captain Mark Watney on this once in a lifetime opportunity to be one of the first civilians to colonize Mars.", price: '99999.95', image: '/assets/mars.jpg'},
+  {title: 'Hotel Winter Housekeeper', description: "FREE! Come stay the winter at the beautiful Stanley hotel in room 237. Just keep the place in one piece and make sure you play; don't do all work.", price: '0', image: '/assets/OverlookHotelShining.fw.png'},
+  {title: 'Crazy Crowded Beach', description: "Everyone is there but YOU! Experience the true peak season from June to August at Crazy Crowded Beach, which is bustling with activity.  This beach will not bore!", price: '1599.99', image: '/assets/Crazy-Crowded-Beach-in-Rio-De-Janeiro-that-is-Insane.jpg'},
+  {title: 'Scenic Helicopter Ride - Nebraska', description: "Circles and squares as far as the eye can see!  Experience Nebraska's great plains like never before! View many historic trails explored by the Lewis and Clark Expedition. Where the West Begins!", price: '9.99', image: '/assets/field.jpg'}
 ])
 
 CategoriesTrip.create([
@@ -50,16 +52,26 @@ CategoriesTrip.create([
 ])
 CategoriesTrip.create([
   {category: Category.find_or_create_by(title: 'Family Fun'), trip: trips[6]},
-  {category: Category.find_or_create_by(title: 'Island Getaway'), trip: trips[6]},
+  {category: Category.find_or_create_by(title: 'Island Getaway'), trip: trips[6]}
 ])
 CategoriesTrip.create([
   {category: Category.find_or_create_by(title: 'Space Travel'), trip: trips[7]},
-  {category: Category.find_or_create_by(title: 'One of a Kind'), trip: trips[7]},
+  {category: Category.find_or_create_by(title: 'One of a Kind'), trip: trips[7]}
 ])
 CategoriesTrip.create([
   {category: Category.find_or_create_by(title: 'Family Fun'), trip: trips[8]},
   {category: Category.find_or_create_by(title: 'Mountains'), trip: trips[8]},
   {category: Category.find_or_create_by(title: 'Bang For Your Buck'), trip: trips[8]}
+])
+CategoriesTrip.create([
+  {category: Category.find_or_create_by(title: 'Family Fun'), trip: trips[9]},
+  {category: Category.find_or_create_by(title: 'Beach'), trip: trips[9]},
+  {category: Category.find_or_create_by(title: 'Water Sports'), trip: trips[9]}
+])
+CategoriesTrip.create([
+  {category: Category.find_or_create_by(title: 'Family Fun'), trip: trips[10]},
+  {category: Category.find_or_create_by(title: 'Xtreme'), trip: trips[10]},
+  {category: Category.find_or_create_by(title: 'Bang For Your Buck'), trip: trips[10]}
 ])
 
 User.create(name: 'Admin Bob', username: 'admin', password: '@dminPa$$word', email: 'admin@email.com', role: 1)
