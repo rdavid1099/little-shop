74edba237589f1be30ff86d5a8ad5e443f21a08e
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-09-21 01:17:51 -0600

    Styling for trips page and show page
    -Cart checkout also styled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,15 +3,15 @@ module ApplicationHelper
     if @cart.total_price.zero?
       'Your Cart is Empty'
     elsif logged_in?
-      link_to 'Checkout', orders_path, method: :post
+      link_to 'Checkout', orders_path, method: :post, class: 'btn btn-defaul btn-style col-md-offset-9'
     else
-      link_to 'Login or Create Account to Checkout', login_path
+      link_to 'Login or Create Account to Checkout', login_path, class: 'btn btn-defaul btn-style col-md-offset-8'
     end
   end
 
   def display_trip_status
     if @trip.status == 'active'
-      button_to 'Add to Cart', cart_path(trip_id: @trip.id), method: :post, class: 'btn btn-success'
+      button_to 'Add to Cart', cart_path(trip_id: @trip.id), method: :post, class: 'btn btn-style btn-success'
     else
       'Trip Expired'
     end
