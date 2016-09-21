385083e4083cc655152bc9905d644e2ace5e9b59
Matt D <MDes41@users.noreply.github.com>
2016-09-21 13:48:58 -0600

    Background added and font styling with picture borders (#73)
    * Styling for trips page and show page
    
    -Cart checkout also styled
    
    * Fix formatting
    
    * fix formatting'
    
    * Fix bottom nav bar'
    
    * Fix texts to find wrapper top nav
    
    * Put grey background in
    
    * Fix rspec work

--- spec/features/visitor_can_view_trip_page_and_add_item_to_cart_spec.rb
+++ spec/features/visitor_can_view_trip_page_and_add_item_to_cart_spec.rb
@@ -8,8 +8,10 @@ RSpec.feature 'Visitor can view trip page and add item to cart' do
 
       expect(page).to have_content('Shopping Cart')
       expect(page).to have_content('Cart: 1')
-      expect(page).to have_content('1 - Bermuda - $199.99')
-      expect(page).to have_content('Total: $199.99')
+      expect(page).to have_content('1 Bermuda')
+      expect(page).to have_content('$199.99')
+      expect(page).to have_content('Total:')
+      expect(page).to have_content('$199.99')
     end
   end
 
@@ -24,8 +26,10 @@ RSpec.feature 'Visitor can view trip page and add item to cart' do
 
       expect(page).to have_content('Cart Items')
       expect(page).to have_content('Cart: 2')
-      expect(page).to have_content('2 - Bermuda - $399.98')
-      expect(page).to have_content('Total: $399.98')
+      expect(page).to have_content('2 Bermuda')
+      expect(page).to have_content('$399.98')
+      expect(page).to have_content('Total:')
+      expect(page).to have_content('$399.98')
     end
   end
 end
