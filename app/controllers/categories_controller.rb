9bc33c2d669ddc106eeb32e972fbf244038f2aad
Ryan Workman <rdavid1099@gmail.com>
2016-09-21 05:50:26 -0600

    Fix minor gatekeeper issue

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,10 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find_by(title: params[:id])
-    @trips = @category.trips
-    render file: 'public/404' if @category.nil?
+    if @category.nil?
+      render file: 'public/404'
+    else
+      @trips = @category.trips
+    end
   end
 end
